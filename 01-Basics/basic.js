9b296e042a63e760f807d4d7c595d7640adb2de6
saalimkhan31 <100254973+saalimkhan31@users.noreply.github.com>
2024-11-23 12:12:52 +0000

    new commit

--- 01-Basics/basic.js
+++ 01-Basics/basic.js
@@ -2,10 +2,17 @@
 // type of undefined is undefined
 // str num bool bigint symbol object  undefined null
 
-let str = 'city';
-let newStr = Number(str)
-console.log(typeof newStr);
-console.log(newStr);
+// let str = undefined;
+// let newStr = Number(str)
+// console.log(typeof newStr);
+// console.log(newStr);
+
+let num =12 ;
+let checkBoolean = Boolean(num)
+console.log(typeof checkBoolean);
+console.log(checkBoolean);
+
+
 
 
 
