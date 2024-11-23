e60d8f19d5ad59594869de6a395854449c50f6d6
saalimkhan31 <100254973+saalimkhan31@users.noreply.github.com>
2024-11-23 12:04:48 +0000

    data types

--- 01-Basics/basic.js
+++ 01-Basics/basic.js
@@ -1,4 +1,12 @@
-console.log('hello');
-console.log('hello');
+// type of null is obj
+// type of undefined is undefined
+// str num bool bigint symbol object  undefined null
+
+let str = 'city';
+let newStr = Number(str)
+console.log(typeof newStr);
+console.log(newStr);
+
+
 
 
